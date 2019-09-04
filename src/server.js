fbcc5e66d04ace115afd6c361be751ca338d954e
Sam Belisle <spb6892@rit.edu>
2019-09-04 13:41:24 -0400

    Update line endings

--- src/server.js
+++ src/server.js
@@ -11,29 +11,29 @@ const onRequest = (request, response) => {
 
   switch (request.url) {
     case '/':
-        htmlHandler.getIndex(request, response);
-        break;
+      htmlHandler.getIndex(request, response);
+      break;
     case '/page2':
-        htmlHandler.getPage2(request, response);
-        break;
+      htmlHandler.getPage2(request, response);
+      break;
     case '/hello':
-        textHandler.getHello(request, response);
-        break;
+      textHandler.getHello(request, response);
+      break;
     case '/time':
-        textHandler.getTime(request, response);
-        break;
+      textHandler.getTime(request, response);
+      break;
     case '/helloJSON':
-        jsonHandler.getHelloJSON(request, response);
-        break;
+      jsonHandler.getHelloJSON(request, response);
+      break;
     case '/timeJSON':
-        jsonHandler.getTimeJSON(request, response);
-        break;
+      jsonHandler.getTimeJSON(request, response);
+      break;
     case '/image':
-        imageHandler.getImage(request, response);
-        break;
+      imageHandler.getImage(request, response);
+      break;
     default:
-        htmlHandler.getIndex(request, response);
-        break;
+      htmlHandler.getIndex(request, response);
+      break;
   }
 };
 
